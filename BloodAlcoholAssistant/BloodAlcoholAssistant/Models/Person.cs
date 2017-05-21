2c6aa48cc5e8c009abcac4479888ec39f0b2d86e
ka-res <kares.inf@gmail.com>
2017-05-21 13:01:39 +0200

    fix typing, add cancel action link

--- BloodAlcoholAssistant/BloodAlcoholAssistant/Models/Person.cs
+++ BloodAlcoholAssistant/BloodAlcoholAssistant/Models/Person.cs
@@ -7,7 +7,7 @@ namespace BloodAlcoholAssistant.Models
         [Range(0.0, 250.0, ErrorMessage = "Waga musi być z przedziału od 0 do 250 kg")]
         public double WeightAsKilo { get; set; }
 
-        [Range(0.0, 100.0, ErrorMessage = "Ilość alkoholi należy podać z przedziału od 0 do 100")]
+        [Range(0, 100, ErrorMessage = "Ilość alkoholi należy podać z przedziału od 0 do 100")]
         public int NumberOfAlcohols { get; set; }
 
         public Gender Sex { get; set; }
